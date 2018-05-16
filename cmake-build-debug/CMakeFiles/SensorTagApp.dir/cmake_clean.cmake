5b9cf79ca4f25c32fe930e2a065b6542b827c207
Patryk <PatFra>
2018-05-16 21:49:48 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/SensorTagApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SensorTagApp.dir/main.cpp.o"
+  "SensorTagApp.pdb"
+  "SensorTagApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SensorTagApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
